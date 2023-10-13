d47f61f67f5a65c41319755ea618c26a1be8f321
w1nniro <144254900+w1nniro@users.noreply.github.com>
2023-10-13 10:17:08 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,10 @@
+﻿program zad3;
+var i:integer;
+begin
+  i:=2;
+  while i<=99 do 
+   begin
+    writeln (i,' ');
+    i:=i+2;
+  end;
+end.
---
+++ 8.pas
@@ -0,0 +1,14 @@
+﻿program zad8;
+var 
+  n,r,g,f:real;
+  begin
+ write('введите число ');
+  read(n);
+  g:=1;
+  repeat
+    g:=g+1/n;
+    n:=n-1;
+  until n=1;
+  write(g);
+end.
+
